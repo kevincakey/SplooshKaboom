04a0e0cf1a67f43b16141e72491d01cc0f296cac
kevincakey <okevin.gao@gmail.com>
2020-12-07 14:31:17 -0800

    TileCreator operation added parameters

--- header/TileCreator.h
+++ header/TileCreator.h
@@ -16,9 +16,9 @@ class TileCreator
 {
 public:
 	virtual Tile* createTile(int x, int y, Squid* s) = 0;
-	string operation()
+	string operation(int x, int y, Squid* s)
 	{
-		Tile* t = this->createTile();
+		Tile* t = this->createTile(x, y, s);
 		string result = "Created with " + t->msg();
 		return result;
 	}
